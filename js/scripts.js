13cc56f25dbf0483a1ba22b1900cbd0e97ad2553
Murutu <murutupeter@email.com>
2019-09-20 12:58:48 +0300

    verify month

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@ function verifyDayValidity() {
     console.log(input);
     if (!parseInt(input) || parseInt(input) <1 || parseInt (input) > 31) {
         alert("Please input a number between 1 and 31 for day");
-        return undefined
+        return undefined;
+    } else {
+        return parseInt(input);
     }
+}
+
+function verifyDayValidity() {
+    console.log("Verify month");
+    var input = document.getElementById("month").value;
+    console.log (input);
+    if 
 }
