ae5515c72d757b0e2459841262c68c1ded44cc72
Murutu <murutupeter@email.com>
2019-09-20 12:51:38 +0300

    input a number between 1 and 31 for day

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 function verifyDayValidity() {
-    console.log
+    console.log("Verify day");
+    var input= document.getElementById("day").value;
+    console.log(input);
+    if (!parseInt(input) || parseInt(input) <1 || parseInt (input) > 31) {
+        alert("Please input a number between 1 and 31 for day");
+        return undefined
+    }
 }
